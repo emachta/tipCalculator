ec26381e34e9de46351f28391dab6e6b231f4f4c
Ennis Machta <esmachta@gmail.com>
2020-01-03 09:56:59 -0800

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Ennis on 1/3/20.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
